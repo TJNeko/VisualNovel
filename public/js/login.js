f2ea34c5bef9d9ff49fc520b03596070fbb4d02b
Kasumi Akiyama★ <shellyakiyama@gmail.com>
2024-06-11 10:31:08 +0900

    this is so far not error

--- public/js/login.js
+++ public/js/login.js
@@ -2,10 +2,10 @@ const loginFormHandler = async (event) => {
   event.preventDefault();
 
   // Collect values from the login form
-  //const email = document.querySelector('#email-login').value.trim();
+  const name = document.querySelector('#name-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
-  if (email && password) {
+  if (name && password) {
     // Send a POST request to the API endpoint
     const response = await fetch('/api/users/login', {
       method: 'POST',
