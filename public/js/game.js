ebc0a70562e29346baa1157e48a337885d92f0b7
Kasumi Akiyama★ <shellyakiyama@gmail.com>
2024-06-13 21:20:59 +0900

    added game.js to intro handlebar

--- public/js/game.js
+++ public/js/game.js
@@ -38,7 +38,38 @@ function getNextStoryPart() {
   // return "..";
 }
 
+const textBox = document.getElementById("user-name"); 
+const button = document.getElementById("name-button");
+const div1 = document.getElementById("first-section");
+const div2 = document.getElementById("second-section");
 
+textBox.addEventListener("keyup", function (event) { 
+
+    // Checking if key pressed is ENTER or not 
+    // if the key pressed is ENTER 
+    // click listener on button is called 
+    if (event.keyCode == 13) { 
+        button.click(); 
+    } 
+}); 
+// This event is fired when button is clicked 
+button.addEventListener("click", function () { 
+    let str = textBox.value; 
+    console.log(str); 
+
+  if (div2.style.display === "none") {
+      div1.style.display = "none";
+      div2.style.display = "block";
+      
+  const nameSection = document.getElementById('name-section');
+  const secondParagraph = nameSection.textContent + str + "!";
+  nameSection.innerHTML = secondParagraph;
+
+  } else {
+      div1.style.display = "block";
+      div2.style.display = "none";
+  }
+}); 
 
 
 // Initialize the game when the DOM is fully loaded
