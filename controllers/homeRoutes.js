efc3c8d4747ada67bc7987186fbcc441505baa14
Nekoteej <161190070+Nekoteej@users.noreply.github.com>
2024-06-17 18:25:56 -0600

    more

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,7 +6,7 @@ const withAuth = require("../utils/auth");
 router.get("/", async (req, res) => {
   try {
     // Pass serialized data and session flag into template
-    res.render("homepage", {
+    res.render("/homepage", {
       //  stories,
       logged_in: req.session.logged_in,
     });
